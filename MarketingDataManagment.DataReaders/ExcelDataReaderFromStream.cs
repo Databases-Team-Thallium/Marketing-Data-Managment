25cd857464cff8b9d11d488b85145512bcad1062
Angel Valkov <avalkov@mail.bg>
2015-10-14 22:27:34 +0300

    Introduced PdfWriter
    Introduced PdfWriter

---
+++ MarketingDataManagment.DataReaders/ExcelDataReaderFromStream.cs
@@ -0,0 +1,72 @@
+﻿namespace MarketingDataManagment.DataReaders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.IO;
+
+    using Excel;
+
+    using DataTypes;
+    using StringsUtilities;
+
+    public class ExcelDataReaderFromStream : IDataReader
+    {
+        private readonly long streamLength;
+        private readonly Stream stream;
+        private readonly DateTime streamDate;
+        private readonly MemoryStream memoryStream;
+
+        public ExcelDataReaderFromStream(Stream stream, long streamLength, DateTime streamDate)
+        {
+            this.stream = stream;
+            this.streamLength = streamLength;
+            this.streamDate = streamDate;
+
+            var streamContent = new byte[streamLength];
+            stream.Read(streamContent, 0, (int)streamLength);
+            this.memoryStream = new MemoryStream(streamContent);
+        }
+
+        public TableData ReadData()
+        {
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(this.memoryStream);
+            excelReader.IsFirstRowAsColumnNames = true;
+            DataSet dataSet = excelReader.AsDataSet();
+
+            int rowIndex = 0;
+            var tableData = new TableData();
+
+            while (excelReader.Read())
+            {
+                if (dataSet.Tables[0].Rows[rowIndex].IsNull(0) == true) { break; }
+
+                if (rowIndex == 0)
+                {
+                    for (int i = 0; i < dataSet.Tables[0].Columns.Count; i++)
+                    {
+                        string columnName = dataSet.Tables[0].Columns[i].ColumnName;
+                        string formattedColumnName = TextFormatter.FormatColumnName(columnName);
+                        tableData.AddColumnName(formattedColumnName);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < tableData.ColumnsCount; i++)
+                    {
+                        string formattedColumnName = tableData.GetColumnName(i);
+                        object data = dataSet.Tables[0].Rows[rowIndex].ItemArray[i].ToString();
+                        tableData.AddData(i, data);
+                    }
+                }
+
+                rowIndex++;
+            }
+
+            excelReader.Dispose();
+            this.memoryStream.Dispose();
+
+            return tableData;
+        }
+    }
+}
--- MarketingDataManagment.DataWriters/PdfWriters/PdfTableWriter.cs
+++ MarketingDataManagment.DataWriters/PdfWriters/PdfTableWriter.cs
@@ -1,13 +1,46 @@
 ﻿namespace MarketingDataManagment.DataWriters.PdfWriters
 {
+    using System.IO;
+
     using iTextSharp.text;
     using iTextSharp.text.pdf;
 
-    public class PdfTableWriter
-    {
-        public PdfTableWriter()
-        {
+    using DataTypes;
 
+    public class PdfTableWriter: IDataWriter
+    {
+        private readonly Document document;
+        private readonly PdfWriter writer;
+
+        public PdfTableWriter(string destinationFullPathName)
+        {
+            this.document = new Document();
+
+            var documentFileStream = new FileStream(destinationFullPathName, FileMode.Create);
+            this.writer = PdfWriter.GetInstance(this.document, documentFileStream);
+
+            this.document.Open();
+        }
+
+        public void WriteData(TableData tableData)
+        {
+            var table = new PdfPTable(tableData.ColumnsCount);
+
+            for(int i = 0; i < tableData.ColumnsCount; i++)
+            {
+                table.AddCell(tableData.GetColumnName(i));
+            }
+
+            for(int i = 0; i < tableData.RowsCount; i++)
+            {
+                for(int j = 0; j < tableData.ColumnsCount; j++)
+                {
+                    table.AddCell(tableData.GetData(j, i).ToString());
+                }
+            }
+
+            this.document.Close();
+            this.writer.Close();
         }
     }
 }
