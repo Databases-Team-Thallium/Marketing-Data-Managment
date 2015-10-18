d44e7b65cee01681c66505b6d96afcd4f3a5513c
dushka-dragoeva <d.dragoeva@abv.bg>
2015-10-18 22:59:31 +0300

    Added SalesReport and JsonWriter

---
+++ MarketingDataManagment.DataWriters/JsonWriter.cs
@@ -0,0 +1,45 @@
+﻿namespace MarketingDataManagment.DataWriters
+{
+    using Databases.MSSQL.Reports;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Web.Script.Serialization;
+
+    public class JsonWriters
+    {
+        private const string JsonReportFilePath = @"..\..\..\Exported-Files\Json";
+
+        public static void CreateJsonFiles(List<SalesReport> saleStores)
+        {
+            if (!Directory.Exists(JsonReportFilePath))
+            {
+                Directory.CreateDirectory(JsonReportFilePath);
+            }
+
+            foreach (var productSales in saleStores)
+            {
+                FileStream fs1 = new FileStream(JsonReportFilePath + @"\" + productSales.Sale.SaleId + ".json", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fs1);
+
+                writer.WriteLine(CreateJsonReport(productSales));
+                writer.Close();
+            }
+
+            Process.Start(JsonReportFilePath);
+        }
+
+        public static string CreateJsonReport(SalesReport salesReport)
+        {
+            return new JavaScriptSerializer().Serialize(
+                    new Dictionary<string, string>()
+                        {
+                            { "sale-id", salesReport.SaleId.ToString() },
+                            { "sale-name", salesReport.Sale.SaleName},
+                            { "store-name", salesReport.Store.StoreName },
+                            { "total-quantity-sold", salesReport.TotalQuantitySold.ToString() },
+                            { "total-incomes", salesReport.TotalIncomes.ToString() }
+                        });
+        }
+    }
+}
---
+++ MarketingDataManagment.Databases/MSSQL/Reports/SalesReport.cs
@@ -0,0 +1,18 @@
+﻿namespace MarketingDataManagment.Databases.MSSQL.Reports
+{
+    using Models;
+   
+        public class SalesReport
+        {
+            public int SaleId { get; set; }
+
+            public Sale Sale { get; set; }
+
+            public Store Store { get; set; }
+
+            public int TotalQuantitySold { get; set; }
+
+            public double TotalIncomes { get; set; }
+        }
+    }
+
